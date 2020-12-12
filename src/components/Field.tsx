810e5ea7dc222021e4d6aa954c26fc091dd85028
Talha Aydin <talha.aydin.87@gmail.com>
2020-12-12 16:08:46 +0100

    feat: pause game on blur

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,13 +1,31 @@
 import { GameGrid } from '../ui/GameGrid'
 import { GameBlock } from '../ui/GameBlock'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { getAllBlocks } from '../redux/selectors/entities'
 import { getPoint } from '../utils/point'
 import { useKeyboardControls } from '../hooks/useKeyboardControls'
+import { useEffect } from 'react'
+import { setStatus } from '../redux/actions/game'
+import { GameStatus } from '../redux/reducers/game'
+import { getStatus } from '../redux/selectors/game'
 
 export const Field: React.FC = () => {
+  const dispatch = useDispatch()
+  const status = useSelector(getStatus)
   const allBlocks = useSelector(getAllBlocks)
   useKeyboardControls()
+
+  useEffect(() => {
+    const handler = () => {
+      if (status === GameStatus.ACTIVE) {
+        dispatch(setStatus(GameStatus.PAUSED))
+      }
+    }
+    window.addEventListener('blur', handler)
+    return () => {
+      window.removeEventListener('blur', handler)
+    }
+  }, [dispatch, status])
 
   return (
     <GameGrid width={10} height={20}>
