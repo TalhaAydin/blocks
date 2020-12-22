dc192ef33052f3cd86947b16e975a152e0b74d6f
Talha Aydin <talha.aydin.87@gmail.com>
2020-12-22 10:41:13 +0100

    fix: improve game speed

--- src/utils/game.ts
+++ src/utils/game.ts
@@ -7,4 +7,4 @@ export const getLevel = (lineClears: number[]) =>
   1 + Math.floor(lineClears.reduce((s, c) => s + c, 0) / 10)
 
 export const getDescentDelay = (level: number) =>
-  Math.max(100, 1000 - (level - 1) * 100)
+  Math.max(100, 1000 - (level - 1) * 150)
